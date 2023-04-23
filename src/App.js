2f491b7bac7efd64f5797f9d4090e7f877c01933
BinhodevTrybe <binhuhue@gmail.com>
2023-04-23 17:39:47 -0300

    Second Requirement Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -7,13 +8,58 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import { createUser } from './services/userAPI';
+
+const numberThree = 3;
 
 class App extends React.Component {
+  state = {
+    nameLogin: '',
+    loading: false,
+    redirect: false,
+  };
+
+  sendLogin = async () => {
+    const { nameLogin } = this.state;
+    const savedName = {
+      name: nameLogin,
+    };
+    this.setState({
+      objectss: savedName,
+      loading: true,
+    });
+    await createUser(savedName);
+    this.setState({
+      loading: false,
+      redirect: true,
+    });
+  };
+
+  onInputChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+    console.log(value);
+  };
+
   render() {
+    const { nameLogin, objectss, loading, redirect } = this.state;
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={ Login } />
+          <Route
+            exact
+            path="/"
+            render={ () => (<Login
+              nameLogin={ nameLogin }
+              onInputChange={ this.onInputChange }
+              disbaleOrNo={ nameLogin.length < numberThree }
+              sendLogin={ this.sendLogin }
+              objectss={ objectss }
+              loading={ loading }
+            />) }
+          />
           <Route path="/search" component={ Search } />
           <Route path="/album/:id" component={ Album } />
           <Route path="/favorites" component={ Favorites } />
@@ -21,6 +67,7 @@ class App extends React.Component {
           <Route path="/profile/edit" component={ ProfileEdit } />
           <Route path="/*" component={ NotFound } />
         </Switch>
+        {redirect && <Redirect to="/search" />}
       </Router>
     );
   }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends Component {
   render() {
+    const { nameLogin, onInputChange, disbaleOrNo, sendLogin, loading } = this.props;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <form action="">
+          <input
+            data-testid="login-name-input"
+            value={ nameLogin }
+            name="nameLogin"
+            type="text"
+            onChange={ onInputChange }
+          />
+          <button
+            onClick={ sendLogin }
+            disabled={ disbaleOrNo }
+            type="button"
+            data-testid="login-submit-button"
+          >
+            Entrar
+          </button>
+        </form>
+        { loading && <p>Carregando...</p> }
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  nameLogin: PropTypes.string,
+  onInputChange: PropTypes.func,
+  disbaleOrNo: PropTypes.bool,
+  sendLogin: PropTypes.func,
+  loading: PropTypes.bool,
+}.isRequired;
 
 export default Login;
