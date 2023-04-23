16d9f4ed8ba6ab82ceace737aa4a4aad9959a5a0
BinhodevTrybe <binhuhue@gmail.com>
2023-04-23 18:42:04 -0300

    Fourth Requirement Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -30,6 +31,9 @@ class Header extends Component {
       >
         {loading && <Loading />}
         <p data-testid="header-user-name">{ name }</p>
+        <Link data-testid="link-to-search" to="/search">Search</Link>
+        <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+        <Link data-testid="link-to-profile" to="/profile">Profile</Link>
       </header>
     );
   }
